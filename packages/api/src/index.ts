b74b8fad1f8cb16ee1a41129d98e722e5d1b350a
Lan Phan <lanc_phan@hotmail.com>
2020-03-21 11:31:10 -0400

    add middlewareLogger and app.use()

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -11,14 +11,25 @@ const app = express();
  * 3. Next
  */
 
-const firstMiddleware: RequestHandler = (req, res, next) => {
-console.log('Getting url', req.url);
-res.send('Hello');
-    // next();
+ const middlewareLogger: RequestHandler = (req, _res, next) => {
+    console.log('Getting url', req.url);
+    next();
+ }
+
+const firstMiddleware: RequestHandler = (_req, res) => {
+res.send('Send 1');
 }
+
+const secondMiddleware: RequestHandler = (_req, res) => {
+res.send('Send 2');
+}
+
+// Use the middleware for ALL requests (Includes, get, post, put, any url)
+app.use(middlewareLogger);
 
 // Defining a new pipe
 app.get('/', firstMiddleware);
+app.get('/two', secondMiddleware);
 
 // Running the web server on port 9999
 app.listen(9999);
