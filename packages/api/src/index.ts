0e4a870da4b5bc70181cb0989e86c2cb5d1e9abc
Lan Phan <lanc_phan@hotmail.com>
2020-03-21 10:47:14 -0400

    add an express server running on localhost:9999

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,9 @@
+import express from 'express';
 console.log('We will write our API server here');
+
+// Creaed an INSTANCE of an API
+const app = express();
+
+// Running the web server on port 9999
+app.listen(9999);
+console.log('API running on localhost:9999');
