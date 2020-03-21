496aef7d5acf48b8ed5eefe81bbb9ea0980cd1ae
Lan Phan <lanc_phan@hotmail.com>
2020-03-21 11:22:40 -0400

    create our first get route, which is simply the '/'

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,24 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
 console.log('We will write our API server here');
 
 // Creaed an INSTANCE of an API
 const app = express();
+
+/**
+ * Each middleware takes 3 parameters
+ * 1. Requst
+ * 2. Response
+ * 3. Next
+ */
+
+const firstMiddleware: RequestHandler = (req, res, next) => {
+console.log('Getting url', req.url);
+res.send('Hello');
+    // next();
+}
+
+// Defining a new pipe
+app.get('/', firstMiddleware);
 
 // Running the web server on port 9999
 app.listen(9999);
